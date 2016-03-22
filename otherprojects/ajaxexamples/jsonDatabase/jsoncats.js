6b40cfbabe6269189a0ecc1d6c06d0509d236f7f
canteranatasha <canteranatasha@gmail.com>
2016-03-22 09:12:11 -0400

    class

---
+++ otherprojects/ajaxexamples/jsonDatabase/jsoncats.js
@@ -0,0 +1,69 @@
+[
+{
+"type":"Calico",
+"name":"Paws",
+"gender":"Male",
+"image":"images/cat1.jpg",
+"comments" : [
+  {
+    "username":"AboveAlpha",
+    "stars": 3,
+  "comment":"Loved this cat!"
+},{
+  "username":"Brushtag",
+  "stars": 4,
+"comment":"Fun to play with!"
+},{
+  "username":"Raventuer",
+  "stars": 3,
+"comment":"Destroyed my house but I still liked him for some reason?"
+}
+]
+},
+{
+"type":"Tabby",
+"name":"Mittens",
+"gender":"Female",
+"image":"images/cat2.jpg",
+"comments" : [
+  {
+    "username":"Dragonco",
+    "stars": 5,
+  "comment":"Seriously loved this cat!"
+},
+{
+  "username":"Helloword",
+  "stars": 3,
+"comment":"Eh, she was alright."
+},
+{
+  "username":"Flowergirl02",
+  "stars": 4,
+"comment":"Loveable and furry."
+}
+]
+},
+{
+"type":"Siamese",
+"name":"Rob",
+"gender":"Male",
+"image":"images/cat3.jpg",
+"comments" : [
+  {
+    "username":"Hola33",
+    "stars": 5,
+  "comment":"Never want him to leave!"
+  },
+  {
+    "username":"Sunshinegal003",
+    "stars": 2,
+  "comment":"Meh.."
+  },
+  {
+    "username":"catlove22",
+    "stars": 4,
+  "comment":"Loved this cat..."
+  }
+]
+}
+]
