9ce037cf96a4e8818338d7f98cfca7168cda3057
canteranatasha <canteranatasha@gmail.com>
2016-04-05 09:46:17 -0400

    k

--- otherprojects/finalProject/js/getNav.js
+++
@@ -1,8 +0,0 @@
-$.get("http://canteranatasha.github.io/myClonedFolder/otherProjects/finalProject/partials/nav.html", function(data){
-     
-$(document).ready(function() {
-        
-     $(".container").prepend(data);
-     $(".container").fadeIn();
-     }) 
-})
