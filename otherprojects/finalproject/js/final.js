b782aa9e25f22ff7263f69795af7460a5dd1790e
canteranatasha <canteranatasha@gmail.com>
2016-04-08 13:01:28 -0400

    ok

--- otherprojects/finalproject/js/final.js
+++ otherprojects/finalproject/js/final.js
@@ -109,7 +109,7 @@ $(document).ready(function() {
 
             alert("Sending to database " + JSON.stringify(order));
             $("#successMsg").html("Order Received!<br/><br/>" +
-              order.catSelect + " will be delivered on " +
+              order.catSelect + " will be delivered on " + 
               order.startRentDate +
               "<img id='paws' src='images/catPaws.jpeg'>");
 
